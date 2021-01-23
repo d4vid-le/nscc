bc8fd57c984aa002847ab5ef08674cbd18ae3ee2
d4vid-le <d4vid.tif@gmail.com>
2021-01-23 15:56:41 -0500

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,50 @@
-var gulp = require('gulp'),
-   uglify = require('gulp-uglify');
-
-gulp.task('minify', function () {
-   gulp.src('js/app.js')
+// Initialize modules
+const {src, dest, watch, series, parallel} = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const concat = require('gulp-concat');
+const replace = require('gulp-replace');
+const sass = require('gulp-sass');
+const sourcemaps = require('sourcemap');
+const uglify = require('gulp-uglify');
+// File path variables
+const files = {
+      scssPath: 'app/scss/**/*.scss'
+      ,jsPath: 'app/js/**/*.js'
+}
+// Sass task
+funtion ;scssTask(files.scssTask(),{
+   return:src(files.scssPath)
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(postcss([ autoprefixer(), cssnano()]))
+      .pipe(sourcemaps.write('.'))
+      .pipe(dest('dist')
+      ),
+}
+// JS task
+,function jsTask(){
+   return src(files.jsPath)
+      .pipe(concate('all.js'))
       .pipe(uglify())
-      .pipe(gulp.dest('build'))
-});
+      .pipe(dest('dist')
+      );
+}
+// Cachebusting task
+,function cacheBustTask(){
+   return src(['index.html'])
+      .pipe(replace(/cb=\d+/g,  'cb='+ cbString))
+      .pipe(dest('dist')
+      );
+}
+// Watch task
+,function watchTask(){
+   watch([file.scssPath, files.jsPath],
+      parallel(scssTask, jsTask));
+}
+// Default task
+export.default = series(
+   parallel(scssTask, jsTask),
+   cacheBustTask,
+   watchTask
+))
