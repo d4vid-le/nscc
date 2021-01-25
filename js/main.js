8aa90031dfb28ab9a3e202050437693c0fa9daf5
d4vid-le <d4vid.tif@gmail.com>
2021-01-24 21:48:56 -0500

    commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* 
+   Author:
+   Created: 
+   Description: Custom Main JS file
+*/
+/*
+(function($) {
+
+$(window).on('load', function() {
+    var preloaderFadeOutTime = 500;
+    function hidePreloader() {
+        var preloader = $('#preloader');
+        setTimeout(function() {
+            preloader.fadeOut(preloaderFadeOutTime);
+        }, 500);
+    }
+    hidePreloader();
+    });  */
